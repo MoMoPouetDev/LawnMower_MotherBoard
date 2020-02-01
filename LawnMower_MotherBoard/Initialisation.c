4d9734504f1474ef29962cfed283b7b435e4bdb4
morgan venandy <morgan.venandy@gmail.com>
2020-02-01 12:19:23 +0100

    Implement Init IO

--- LawnMower_MotherBoard/Initialisation.c
+++ LawnMower_MotherBoard/Initialisation.c
@@ -23,17 +23,47 @@ void Initialisation()
 
 void InitIO()
 {
-    DDRC |= (1<<DDC0) | (1<<DDC1); //LED Moteur - ON : PINC0 ; OFF : PINC1
-    DDRC |= (1<<DDC2) | (1<<DDC3); //LED Vitesse - 1 : PINC2 ; 2 : PINC3
-    DDRC |= (1<<DDC4) | (1<<DDC5); //LED Sens - UP : PINC4 ; DOWN : PINC5
-    DDRD |= (1<<DDD3); //Commande Lumiere - ON/OFF : PIND3
-    DDRD |= (1<<DDD4) | (1<<DDD5); //Commande Moteur droite - PWM : PIND5 ; Normal :PIND4
-    DDRD |= (1<<DDD6) | (1<<DDD7); //Commande Moteur gauche - PWM : PIND6 ; Normal : PIND7
+/***** PORT B *****/
+    DDRB = 0x00;
+    //DDRB |= (0<<DDB0); // Bouton Poussoir
+    DDRB |= (1<<DDB1) | (1<<DDB2) | (1<<DDB4); // LED d'état Vert - Orange - Rouge
+    DDRB |= (1<<DDB3); // Commande PWM Avant Moteur 2
+    DDRB |= (1<<DDB5); // Commande Relais Alim Moteur
+    DDRB |= (1<<DDB6) | (1<<DDB7); // Commande Moteur Lame Enable - ON
     
-    PORTC &= ~(1<<PORTC0) & ~(1<<PORTC1) & ~(1<<PORTC2) & ~(1<<PORTC3) & ~(1<<PORTC4) & ~(1<<PORTC5);
-    PORTD &= ~(1<<PORTD3);
-    PORTD |= (1<<PORTD4) | (1<<PORTD7); // PNP a 1 pour les desactiver
-    PORTD &= ~(1<<PORTD5) & ~(1<<PORTD6);
+    PORTB = 0x00;
+    PORTB |= (1<<PORTB0); // Pull-Up Bouton Poussoir
+    // PORTB &= ~(1<<PORTB1) & ~(1<<PORTB2) & ~(1<<PORTB4); // Force à 0 LED
+    // PORTB |= (1<<PORTB3); // Moteur 2 Avant
+    // PORTB |= (1<<PORTB5); // Commande Relais
+    // PORTB |= (1<<PORTB6); // Enable Moteur Lame
+    // PORTB |= (1<<PORTB7); // Moteur Lame
+    
+/***** PORT C *****/
+    DDRC = 0x00;
+    //DDRC |= (1<<DDC0) | (1<<DDC1); // ADC - Detection cable droite et gauche
+    //DDRC |= (1<<DDC2) | (1<<DDC3); // TBD
+    DDRC |= (1<<DDC4) | (1<<DDC5); // Config I2C SDA - SCL
+    //DDRC |= (1<<DDC6); // TBD
+    
+    PORTC = 0x00;
+    //PORTC &= ~(1<<PORTC0) & ~(1<<PORTC1); // ADC - No Pull-Up
+    PORTC |= (1<<PORTC2) | (1<<PORTC3); // TBD - Pull-Up
+    //PORTC &= ~(1<<PORTC4) & ~(1<<PORTC5); // I2C - Force à 0
+    PORTC |= (1<<PORTC6); // TBD - Pull-Up
+    
+/***** PORT D *****/
+    DDRD = 0x00;
+    DDRD |= (1<<DDD1); //| (0<<DDD0); // UART - TXD - RXD
+    DDRD |= (1<<DDD2) | (1<<DDD3); // Commande Enable - PWM Arriere Moteur 2
+    DDRD |= (1<<DDD4) | (1<<DDD5) | (1<<DDD6); // Commande Enable - PWM Arriere - PWM Avant Moteur 1
+    //DDRD |= (0<<DDD7); // Bouton Poussoir
+    
+    PORTD = 0x00;
+    //PORTD |= (1<<PORTC0) | (1<<PORTC1); // UART - RX No Pull-Up - TX
+    //PORTD |= (1<<PORTD2) | (1<<PORTD3); // Commande Enable - PWM Arriere Moteur 2 - Force à 0
+    //PORTD |= (1<<PORTD4) | (1<<PORTD5) | (1<<PORTD6); // Commande Enable - PWM Arriere - PWM Avant Moteur 1
+    PORTD |= (1<<PORTD7); // Pull-Up Bouton Poussoir
 }
 
 void InitInterrupt()
