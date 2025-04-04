1caf6cc9fc0883b6d3128d07181d405f85e3768d
mvenandy <mvenandy@erlab.net>
2025-04-04 08:56:31 +0200

    update wdt

--- LawnMower_MotherBoard/LLD/LLD_WDT/LLD_WDT.c
+++ LawnMower_MotherBoard/LLD/LLD_WDT/LLD_WDT.c
@@ -9,14 +9,9 @@
 /*! ... INCLUDES ...                                                        */
 /*--------------------------------------------------------------------------*/
 
-#define LLD_WDT_USE_LOCALS
 #include <stdint.h>
 #include <string.h>
 #include <stdbool.h>
-
-#include "MIMXRT1062.h"
-#include "MIMXRT1062_features.h"
-#include "fsl_clock.h"
 
 #include "LLD_WDT.h"
 
@@ -24,20 +19,13 @@
 /* ... DATATYPES ...                                                   		*/
 /*--------------------------------------------------------------------------*/
 
-/*! @name Refresh sequence */
-/*@{*/
-#define WDOG_REFRESH_KEY (0xAAAA5555U)
-/*@}*/
-
 /*--------------------------------------------------------------------------*/
 /*! ... LOCAL FUNCTIONS DECLARATIONS ...                                    */
 /*--------------------------------------------------------------------------*/
 
-
 /*--------------------------------------------------------------------------*/
 /*! ... LOCAL FUNCTIONS DEFINITIONS ...                                     */
 /*--------------------------------------------------------------------------*/
-
 
 /*--------------------------------------------------------------------------*/
 /*! ... GLOBAL FUNCTIONS DEFINITIONS ...                                    */
@@ -51,19 +39,11 @@
 **/
 void LLD_WDT_Init(uint8_t u8_OutputValue)
 {
-    uint16_t u16_Value        = 0U;
-    uint32_t u32_primaskValue = 0U;
-
-    /* Enable the Watchdog */
-    u16_Value = WDOG_WCR_WDE(true) | WDOG_WCR_WT(u8_OutputValue) | WDOG_WCR_SRS_MASK | WDOG_WCR_WDA_MASK;
-
-    /* Set configuration */
-    CLOCK_EnableClock(kCLOCK_Wdog1);
-
-    /* Disable the global interrupt to configure watchdog */
-    u32_primaskValue = DisableGlobalIRQ();
-    WDOG1->WCR    = u16_Value;
-    EnableGlobalIRQ(u32_primaskValue);
+    cli();
+	wdt_reset();
+	WDTCSR |= (1<<WDCE) | (1<<WDE);
+	WDTCSR = (1<<WDIE) | (1<<WDP2) | (1<<WDP1); //1s
+	sei();
 }
 
 
@@ -76,13 +56,7 @@ void LLD_WDT_Init(uint8_t u8_OutputValue)
 **/
 void LLD_WDT_Refresh(void)
 {
-    uint32_t u32_primaskValue = 0U;
 
-    /* Disable the global interrupt to protect refresh sequence */
-    u32_primaskValue = DisableGlobalIRQ();
-    WDOG1->WSR    = WDOG_REFRESH_KEY & 0xFFFFU;
-    WDOG1->WSR    = (WDOG_REFRESH_KEY >> 16U) & 0xFFFFU;
-    EnableGlobalIRQ(u32_primaskValue);
 }
 
 
--- LawnMower_MotherBoard/main.c
+++ LawnMower_MotherBoard/main.c
@@ -1,88 +1,13 @@
 #include <stdio.h>
 #include <util/delay.h>
-#include <avr/wdt.h>
 
-#include "constant.h"
-#include "Initialisation.h"
-#include "mower.h"
-#include "status.h"
-#include "pwm.h"
+#include "FSM_Main.h"
 
-int main(void) {
-
-    Initialisation();
-
+int main(void) 
+{
 	FSM_Main_Init();
     FSM_Main();
+	
     return 0 ;
 }
-/*
-	while(!_uBpStart);
-	_uBpStart = 0;
-	
-	while (1) {
-		wdt_reset();
-		STATUS_updateStatus();
-		STATUS_sendStatus();
-		
-        if(isDocking())
-        {
-			MOWER_updateBladeState(OFF);
-			
-			PWM_stop();
-			
-            if(isCharging())
-            {
-				_eEtatMower = EN_CHARGE;
-				
-				if(_uBpForceStart && (_eEtatRain == OFF)) {
-					_eEtatMower = TACHE_EN_COURS;
-					_uBpStop = 0;
-					_uBpForceStart = 0;	
-					_eErrorMower = NTR;
-					MOWER_leaveDockCharger();
-				}
-            }
-            else if(isTimeToMow() && (_eEtatRain == OFF))
-            {
-				_eEtatMower = TACHE_EN_COURS;
-				_uBpStop = 0;
-				_eErrorMower = NTR;	
-				MOWER_leaveDockCharger();	
-            }
-			else
-				_eEtatMower = PAS_DE_TACHE_EN_COURS;
-        }
-		else if(_uBpStart && (!_uBpStop)) {
-			_eEtatMower = PAUSE;
-			MOWER_updateBladeState(OFF);
-			_eErrorMower = NTR;
-			
-			PWM_stop();
-		}
-		else if(isEnoughCharged() == -1) {
-			MOWER_updateBladeState(OFF);
-			_eEtatMower = RETOUR_STATION;
-			
-			PWM_stop();
-		}
-        else if(isEnoughCharged() && (!isRaining()) && isTimeToMow() && (!(_uBpStart && _uBpStop)))
-        {
-			_eEtatMower = TACHE_EN_COURS;
-						
-            MOWER_startMower();
-        }
-        else
-        {
-			_eEtatMower = RETOUR_STATION;
-			MOWER_updateBladeState(OFF);
-			
-            MOWER_goDockCharger();
-			
-			_uBpStop = 0;
-			_uBpStart = 0;
-        }
-	}
-	return 0; // never reached
-}
-*/
+
